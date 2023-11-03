a6c51defaac73875f846016fc568e671562b8895
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2023-11-03 12:35:18 +0100

    Add Data annotation, DbContext, Controller.

---
+++ Data/Garage2_0Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Garage2._0.Models.Entities;
+
+namespace Garage2._0.Data
+{
+    public class Garage2_0Context : DbContext
+    {
+        public Garage2_0Context (DbContextOptions<Garage2_0Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ParkedVehicle> ParkedVehicles { get; set; } = null!;
+    }
+}
