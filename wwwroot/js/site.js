fbd780ece02de86ce3199b577a0c230aad0d2f27
e16johka <e16johka@student.his.se>
2023-11-03 14:05:50 +0100

    Added popup box replacement script  and branch

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/*SweetAlert PopUp Replacement*/
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.9.0/dist/sweetalert2.all.min.js"></script>
