58417fb702146ec98875a2ea704eaab288730d44
e16johka <e16johka@student.his.se>
2023-11-03 10:09:39 +0100

    Added bootstrap

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Javascript Bootstrap from: https://sweetalert2.github.io/recipe-gallery/
+<script src="sweetalert2.all.min.js"></script>
