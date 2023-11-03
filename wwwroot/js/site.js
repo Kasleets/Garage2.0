5855268917c3b145ca74974a61a50c06cd984e21
e16johka <e16johka@student.his.se>
2023-11-03 11:52:43 +0100

    Added js code for a popup window (bootstrap) that simply says an action was successful which could be used for the "Feedback" part of the assignment

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-//Javascript Bootstrap from: https://sweetalert2.github.io/recipe-gallery/
+﻿//Javascript Bootstrap from: https://sweetalert2.github.io/recipe-gallery/
 <script src="sweetalert2.all.min.js"></script>
+
+//Following code is a bootstrap that presents a popup window.
+//Default text output is 'Signed in successfully' but could be used for the 'Feedback' task in Exercise 12
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 2500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
+
+Toast.fire({
+    icon: 'success',
+    title: 'Vehicle data updated successfully'
+})
