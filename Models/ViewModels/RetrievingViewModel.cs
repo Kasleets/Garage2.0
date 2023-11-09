b56da870ce7790c084a7959b626f21784527a471
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-11-09 11:06:58 +0100

    Update RetrievingViewModel.cs

--- Models/ViewModels/RetrievingViewModel.cs
+++ Models/ViewModels/RetrievingViewModel.cs
@@ -4,7 +4,8 @@ namespace Garage2._0.Models.ViewModels
 {
     public class RetrievingViewModel
     {
-        [RegularExpression(@"^[A-Z]{3}\d{3}$", ErrorMessage = "Please enter a valid registration number with 3 uppercase letters and 3 numbers.")]
+        //[RegularExpression(@"^[A-Z]{3}\d{3}$", ErrorMessage = "Please enter a valid registration number with 3 uppercase letters and 3 numbers.")]
+        [RegularExpression("^[A-Z0-9]*$")]
         public string? RegistrationNumber { get; set; }
     }
 }
